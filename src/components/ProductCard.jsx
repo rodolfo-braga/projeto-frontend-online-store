9b5d555e19c180c713299293f8761ed75720d16c
eliHC <leonardohcds@gmail.com>
2021-09-20 18:32:36 -0300

    commit pre merge

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class ProductCard extends Component {
   render() {
+    const { product: { title, id, thumbnail, price } } = this.props;
+
     return (
-      <div />
+      <div data-testid="product" id={ id }>
+        <Link
+          data-testid="product-detail-link"
+          to={ `product/details/${id}` }
+        />
+      </div>
     );
   }
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 import ProductCard from '../components/ProductCard';
+import Loading from '../components/Loading';
 
 export default class Home extends Component {
   constructor() {
@@ -8,14 +10,53 @@ export default class Home extends Component {
 
     this.state = {
       isLoading: false,
+      category: 'Accesorios para Vehículos', // < LEMBRAR DE ATUALIZAR
       searchText: '',
+      productsResponse: [],
     };
+  }
+
+  componentDidMount() {
+    // const { isLoading } = this.state;
+
+    // if (isLoading) {
+    //   this.getProducts();
+    // }
+  }
+
+  getProducts = async () => {
+    const { category, searchText } = this.state;
+    console.log('getprodudcts');
+
+    const productsResponse = await getProductsFromCategoryAndQuery(category, searchText);
+    this.setState({
+      isLoading: false,
+      productsResponse });
   }
 
   handleInputChange = ({ target: { name, value } }) => this.setState({ [name]: value });
 
+  handleClick = () => {
+    const { category, searchText } = this.state;
+    console.log('handleclick');
+
+    this.setState({ isLoading: true });
+    this.getProducts(category, searchText);
+  }
+
+  renderResults = () => {
+    const { productsResponse } = this.state;
+
+    if (!productsResponse.length) {
+      return <p>Nenhum produto foi encontrado</p>;
+    }
+
+    return productsResponse.map((product) => <ProductCard product={ product } />);
+  }
+
   render() {
-    const { handleInputChange, state: { searchText } } = this;
+    const { handleInputChange, handleClick,
+      state: { isLoading, searchText } } = this;
 
     return (
       <main>
@@ -26,19 +67,21 @@ export default class Home extends Component {
         <form>
           <input
             type="text"
-            name="queryInput"
+            name="searchText"
             id="query-input"
             value={ searchText }
             onChange={ handleInputChange }
             data-testid="query-input"
           />
           <button
-            type="button"
+            type="submit"
             data-testid="query-button"
+            onClick={ handleClick }
           >
             Pesquisar
           </button>
         </form>
+        { isLoading && <Loading /> }
       </main>
     );
   }
