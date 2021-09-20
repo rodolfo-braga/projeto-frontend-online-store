14a65a9b858fc85f703ca8c8dc3b871523340571
eliHC <leonardohcds@gmail.com>
2021-09-20 13:28:31 -0300

    requisito 3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
   render() {
     return (
-      <div data-testid="home-initial-message">
-        Digite algum termo de pesquisa ou escolha uma categoria.
-      </div>
+      <main>
+        <Link to="/cart" data-testid="shopping-cart-button">Carrinho</Link>
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      </main>
     );
   }
 }
