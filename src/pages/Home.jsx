73c1dacdba7e4acacb15d45c0c82562ba55e33b5
Wallacy Francis <wallacy_francis@hotmail.com>
2021-09-20 20:56:45 -0300

    Resolvendo requisito 6

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,34 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { getCategories } from '../services/api';
 
 export default class Home extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categoriesList: '',
+    };
+    this.requestCategories = this.requestCategories.bind(this);
+  }
+
+  requestCategories() {
+    const categories = getCategories();
+    categories.then((results) => {
+      this.setState({ categoriesList: results });
+    });
+  }
+
   render() {
+    const { categoriesList } = this.state;
     return (
       <main>
         <Link to="/cart" data-testid="shopping-cart-button">Carrinho</Link>
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <aside>
+          Categorias
+        </aside>
       </main>
     );
   }
